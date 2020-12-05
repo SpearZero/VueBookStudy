a9248681367fed4ad93395590f4ba49df96bd535
SpearZero <spearzero0@gmail.com>
2020-12-05 16:10:04 +0900

    [커피 한 잔 마시며 끝내는 Vue.JS] 게시판 실습

---
+++ README.md
@@ -0,0 +1,11 @@
+# 커피 한 잔 마시며 끝내는 Vue.JS 실습
+
+## 게시판 프로젝트 실습
+
+### 책의 소스코드 주소
+
+> https://github.com/CanDoVueJS
+
+### 이 저장소의 소스코드 원본 주소
+
+> https://github.com/CanDoVueJS/board-example-frontend
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="app-header">
+        <h1>Community</h1>
+        <div v-if="isAuthorized">
+            <strong>
+                <button @click="toggle">{{ me.name }} 환영합니다</button>
+                <i v-if="!isActive" class="fas fa-sort-down"></i>
+                <i v-else class="fas fa-sort-up"></i>
+            </strong>
+            <ul v-if="isActive">
+                <li><button @click="onClickSignout">로그아웃</button></li>
+            </ul>
+        </div>
+        <div v-else>
+            <router-link :to="{ name : 'Signin' }">로그인</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState } from 'vuex'
+
+export default {
+    name: 'AppHeader',
+    computed: {
+        ...mapGetters([ 'isAuthorized' ]),
+        ...mapState([ 'me' ])
+    },
+    data() {
+        return {
+            isActive: false
+        }
+    },
+    methods: {
+        toggle() {
+            this.isActive = !this.isActive
+        },
+        onClickSignout() {
+            this.signout()
+            this.$router.push({ name: 'PostListPage' })
+        },
+        ...mapActions([ 'signout' ])
+    },
+}
+</script>
